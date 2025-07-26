7c3a329b720eff040687aa853ec8065f44e501dd
Sophy <snasambu40@gmail.com>
2025-07-26 18:53:02 +0300

    Fix: use string-based operations (add, subtract, multiply, divide)

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,15 @@
 def perform_operation(num1, num2, operation):
-    if operation == '+':
+    if operation == "add":
         return num1 + num2
-    elif operation == '-':
+    elif operation == "subtract":
         return num1 - num2
-    elif operation == '*':
+    elif operation == "multiply":
         return num1 * num2
-    elif operation == '/':
+    elif operation == "divide":
         if num2 == 0:
             return "Cannot divide by zero"
         return num1 / num2
     else:
         return "Invalid operation"
+
 
