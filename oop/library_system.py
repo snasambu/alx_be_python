9d9ec17c6b2653585d029c8384933d3744c23e64
Sophy <snasambu40@gmail.com>
2025-08-05 15:03:05 +0300

    Fix: Add correct class structure and output for OOP task

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,7 +3,7 @@ class Book:
         self.title = title
         self.author = author
 
-    def get_info(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
@@ -12,7 +12,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def get_info(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -21,7 +21,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_info(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
@@ -30,9 +30,8 @@ class Library:
         self.books = []
 
     def add_book(self, book):
-        if isinstance(book, Book):
-            self.books.append(book)
+        self.books.append(book)
 
     def list_books(self):
         for book in self.books:
-            print(book.get_info())
+            print(book)
